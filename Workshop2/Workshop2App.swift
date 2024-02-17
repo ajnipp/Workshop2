aa966775a85239c372833b410f6f059e0c9f65a3
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2024-02-16 20:59:39 -0500

    Initial Commit

---
+++ Workshop2/Workshop2App.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop2App.swift
+//  Workshop2
+//
+//  Created by AlecNipp on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Workshop2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
